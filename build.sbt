86a52722a6453129300200815b46b65a2d45a9f2
Paul Butcher <paul@paulbutcher.com>
2012-11-23 10:11:28 +0000

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "hashmap-bug"
+ 
+version := "0.1"
+ 
+scalaVersion := "2.10.0-RC3"
+
+fork := true
+
+javaOptions += "-Xmx4G"
+
+outputStrategy := Some(StdoutOutput)
---
+++ src/main/scala/HashMapBug.scala
@@ -0,0 +1,29 @@
+import io.Source
+
+object HashMapBug {
+
+  def main(args: Array[String]) {
+    load("mobythes.aur")
+  }
+
+  def load(fileName: String) {
+
+    val nodes = new collection.mutable.HashMap[Symbol, Integer]
+    
+    def chunks() = new Iterator[Symbol] {
+      val lines = Source.fromFile(fileName).getLines
+      def hasNext = lines.hasNext
+      def next = (lines.next.split(",") map { s => Symbol(s) }).head
+    }
+      
+    chunks foreach { node =>
+      nodes += (node -> nodes.size)
+    }
+    
+    val graph = Array.ofDim[Int](nodes.size, nodes.size)
+    chunks foreach { node =>
+      if (!nodes.isDefinedAt(node))
+        println(node)
+    }
+  }
+}
